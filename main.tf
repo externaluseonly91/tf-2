69eb6747628e4eb9115bcdfa9f6ee56486b2b2c8
externaluseonly91 <134925902+externaluseonly91@users.noreply.github.com>
2023-08-28 11:02:20 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "eu-central-1"
-  access_key = "xxxxxxxxx"
-  secret_key = "xxxxxxxxx"
+  access_key = ${{ secrets.AWS_ACCESS_KEY_ID }}
+  secret_key = ${{ secrets.AWS_SECRET_ACCESS_KEY }}
 }
 
 terraform {
